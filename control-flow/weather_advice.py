4447e8b666c38ed4de6ffd5a5f4cc22324c95a3b
Mediwynn M Maart <mediwynnm@gmail.com>
2024-06-26 11:53:10 +0200

    removed Array

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -2,13 +2,13 @@ potential_condition = ["sunny" , "rainy" , "cold"]
 current_condition = (input("What's the weather like today? (sunny/rainy/cold): "))
 
 
-if current_condition == potential_condition[0]:
+if current_condition == "sunny":
     print("Wear a t-shirt and sunglasses.")
 
-elif current_condition == potential_condition[1]:
+elif current_condition == "rainy":
     print("Don't forget your umbrella and a raincoat.")
 
-elif current_condition == potential_condition[2]:
+elif current_condition == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 
 else:
