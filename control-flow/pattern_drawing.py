a40854f47601291f3f5bd5454e5a3697de6d4ae0
Mediwynn M Maart <mediwynnm@gmail.com>
2024-06-26 12:33:52 +0200

    incrementation

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,12 +1,19 @@
 userin = int(input("Enter the size of the pattern: "))
-
+i = 0
+j = 0
 block = "*"
 
-while userin >= 0:  
+while i != userin:  
     if userin >= 0:
         for i in range(userin):
+
             for j in range(userin):
                 print(block, end=' ')
+                j = j + 1
+            
+            i = i + 1
             print()
+
+
     else:
         print("Please enter a valid non-negative integer for the size of the grid.")
