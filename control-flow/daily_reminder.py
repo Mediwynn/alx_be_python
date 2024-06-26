57abbc01f3b54757965c3f4dc81b8c388eae09b8
Mediwynn M Maart <mediwynnm@gmail.com>
2024-06-26 22:00:51 +0200

    removed space

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -6,13 +6,13 @@ time_bound = input("Is it time-bound? (yes/no): ")
 
 match priority:
     case 'high':
-        print( "Reminder: ", task, "is a high priority task", end= " ")
+        print("Reminder: ", task, "is a high priority task", end= " ")
 
     case 'medium':
-        print( "Reminder: ", task, "is a medium priority task", end= " ")
+        print("Reminder: ", task, "is a medium priority task", end= " ")
 
     case 'low':
-        print( "Reminder: ", task, "is a low priority task", end= " ")
+        print("Reminder: ", task, "is a low priority task", end= " ")
 
     case _:
         print("Invalid priority level")
