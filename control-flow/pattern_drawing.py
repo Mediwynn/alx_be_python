47bbbcac4e19567ee8842da5b7afbf25b0104321
Mediwynn M Maart <mediwynnm@gmail.com>
2024-06-26 12:05:35 +0200

    while

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,11 +2,11 @@ userin = int(input("Enter the size of the pattern: "))
 
 block = "*"
 
-if userin >= 0:  
-
-    for i in range(userin):
-        for j in range(userin):
-            print(block, end=' ')
-        print()
-else:
-    print("Please enter a valid non-negative integer for the size of the grid.")
+while userin >= 0:  
+    if userin >= 0:
+        for i in range(userin):
+            for j in range(userin):
+                print(block, end=' ')
+            print()
+    else:
+        print("Please enter a valid non-negative integer for the size of the grid.")
