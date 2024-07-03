ec4e9771e00c0d966d5eceabe907e07e1c8f1d24
Mediwynn M Maart <mediwynnm@gmail.com>
2024-07-03 22:32:20 +0200

    init datetime commit

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,29 @@
+import datetime
+add = None
+
+def display_current_datetime():
+    current_date_time = datetime.datetime.now()
+    format_date_time = current_date_time.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current date and time: {format_date_time}")
+
+display_current_datetime()
+
+def ask():
+    days_to_add = input("Enter the number of days to add to the current date: ")
+    return days_to_add
+def integer(numbergiven):
+
+    if numbergiven.isdigit():
+        plus = int(numbergiven)
+        return plus
+    else:
+        print("INVALID INPUT")
+
+
+def calculate_future_date():
+    
+    current_date = datetime.date.today()
+    future_date = current_date + datetime.timedelta(days = integer(ask()))
+    print(f"Future date: {future_date}")
+
+calculate_future_date()
