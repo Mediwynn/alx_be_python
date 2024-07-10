0d2aabdf59b3a9de47b531f32cadc3a50e6df924
Mediwynn M Maart <mediwynnm@gmail.com>
2024-07-10 23:32:01 +0200

    Revert

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,12 +1,14 @@
 class BankAccount:
-    def __init__(self, account_balance):
+    def __init__(self, account_balance, def_val = 0):
         self.account_balance = account_balance
+        self.def_val = def_val
+        
 
     def deposit(amount) -> None:
             self.account_balance += amount
             return amount
 
-    def withdraw(amount) -> None:
+    def withdraw(amount):
         minus = float(amount)
         if minus >= self.account_balance:
             self.account_balance -= minus
