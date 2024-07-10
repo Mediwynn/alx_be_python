7850a4e54925cdf65f48fc830ce570379a460ff5
Mediwynn M Maart <mediwynnm@gmail.com>
2024-07-10 21:33:06 +0200

    checking error for main-0

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,17 +1,32 @@
 class BankAccount:
-    def __init__(self, account_balance, def_bal = 0):
+    def __init__(self, account_balance, def_val = 0):
         self.account_balance = account_balance
         self.def_val = def_val
 
+    def isnum(amount):
+        try:
+            num = float(amount)
+        except ValueError:
+            print(f"Not a valid number provided")
+        else:
+            return True
+        
+
     def deposit(amount):
-        self.account_balance += amount
-        added = f"{self.name} deposited: {amount}. current balance is: {self.account_balance}"
+        if isnum(amount) == True:
+            self.account_balance += amount
+            added = f"{self.name} deposited: {amount}. current balance is: {self.account_balance}"
+            return added
+        else:
+            print("Retry number input")
 
     def withdraw(amount):
-        if amount >= self.account_balance:
-            self.account_balance -= amount
+        minus = float(amount)
+        if minus >= self.account_balance:
+            self.account_balance -= minus
         else:
             print(f"Insufficient funds to withdraw: {amount}")
+
 
     def display_balance():
         Current_Balance = self.account_balance
