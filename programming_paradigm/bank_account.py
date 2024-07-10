c0ccceb734fb0669cd30b2b795a8a5e1b8b1a1bd
Mediwynn M Maart <mediwynnm@gmail.com>
2024-07-10 21:09:57 +0200

    name changed

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,15 +4,15 @@ class BankAccount:
         self.account_balance = account_balance
         self.def_val = def_val
 
-    def depo(amount):
+    def deposit(amount):
         self.account_balance += amount
         added = f"{self.name} deposited: {amount}. current balance is: {self.account_balance}"
 
-    def withd(amount):
+    def withdraw(amount):
         if amount >= self.account_balance:
             self.account_balance -= amount
         else:
             print(f"Insufficient funds to withdraw: {amount}")
 
-    def disp(amount):
+    def display_balance():
         return f"Current balance: {self.account_balance}"
