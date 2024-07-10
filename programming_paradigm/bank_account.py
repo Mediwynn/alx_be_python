0240b44ad804007ecb6ca710247e6d233bca425f
Mediwynn M Maart <mediwynnm@gmail.com>
2024-07-10 21:42:50 +0200

    simplify depo

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,31 +2,17 @@ class BankAccount:
     def __init__(self, account_balance, def_val = 0):
         self.account_balance = account_balance
         self.def_val = def_val
-
-    def isnum(amount):
-        try:
-            num = float(amount)
-        except ValueError:
-            print(f"Not a valid number provided")
-        else:
-            return True
         
 
     def deposit(amount):
-        if isnum(amount) == True:
             self.account_balance += amount
-            added = f"{self.name} deposited: {amount}. current balance is: {self.account_balance}"
-            return added
-        else:
-            print("Retry number input")
 
     def withdraw(amount):
         minus = float(amount)
         if minus >= self.account_balance:
             self.account_balance -= minus
         else:
-            print(f"Insufficient funds to withdraw: {amount}")
-
+           return False
 
     def display_balance():
         Current_Balance = self.account_balance
