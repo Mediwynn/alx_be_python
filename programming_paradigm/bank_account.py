6754abd9fa7d569b8268a870dd74808cc1d0f2c5
Mediwynn M Maart <mediwynnm@gmail.com>
2024-07-11 16:55:41 +0200

    simplified

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,20 +1,19 @@
 class BankAccount:
-    def __init__(account, account_balance, def_val = 0):
-        account.account_balance = account_balance
-        account.def_val = def_val
+    def __init__(self, account_balance, def_val = 0):
+        self.account_balance = account_balance
+        self.def_val = def_val
         
 
-    def deposit(amount, account) -> None:
-            account.account_balance += amount
+    def deposit(amount) -> None:
+            self.account_balance += amount
             return amount
 
     def withdraw(amount):
         minus = float(amount)
-        if minus >= account.account_balance:
-            account.account_balance -= minus
+        if minus >= self.account_balance:
+            self.account_balance -= minus
         else:
            return False
 
     def display_balance():
-        current = account.account_balance
-        return print(f"Current Balance: ${current}")
+        return f"Current Balance: ${current}"
