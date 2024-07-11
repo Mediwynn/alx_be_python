770af405255361a8fdcd3d1daf047b0d1da48d8c
Mediwynn M Maart <mediwynnm@gmail.com>
2024-07-11 21:51:18 +0200

    init library

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,21 @@
+class Book:
+    def __init__(title, author, _is_checked_out):
+        name = title
+        writer = author
+
+        for title in Library._books:
+            if Library._books == title:
+                _is_checked_out = False
+                break
+    
+    def Library():
+        _books = []
+
+        def add_book(title, author):
+            Book.__init__(title, author, None)
+        
+        def check_out_book(title):
+            assf
+
+        def return_book(title):
+            asgf
