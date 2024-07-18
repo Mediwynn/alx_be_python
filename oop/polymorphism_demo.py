586c698f9ea5920380adc8a686291b496fe5d5ff
Mediwynn M Maart <mediwynnm@gmail.com>
2024-07-18 16:54:08 +0200

    initial poly commit

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,32 @@
+class Shape:
+    def __init__(self) -> None:
+        pass
+    
+    def area(self):
+        raise NotImplementedError
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width) -> None:
+        self.length = length
+        self.width = width
+        super().__init__()
+    
+    
+    def area(self):
+        super().area()
+        area = self.length * self.width
+        
+        return f"Area of rectangle: {area}"
+
+
+class Circle(Shape):
+    def __init__(self, radius) -> None:
+        self.radius = radius
+        super().__init__()
+    
+    def area(self):
+        super().area()
+        area = math.pi * self.radius * self.radius
+
+        return f"Area of circle: {area}"
