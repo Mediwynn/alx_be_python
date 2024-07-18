2501210160dc74108e1d467df048246917df027e
Mediwynn M Maart <mediwynnm@gmail.com>
2024-07-18 14:32:22 +0200

    initial commit

---
+++ oop/book_class.py
@@ -0,0 +1,15 @@
+class Book:
+    def __init__(self, title: str, author: str, publication_year: int):
+        self.title = title
+        self.author = author
+        self.year = publication_year
+
+    def __del__(self):
+        
+        return f"Deleting {self.title}"
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+    
+    def __repr__(self) -> str:
+        return f"Book('{self.title}', '{self.author}', {self.year})"
