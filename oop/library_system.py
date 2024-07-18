c6ddb02d040759d694bf52ac39a94390a6c847d9
Mediwynn M Maart <mediwynnm@gmail.com>
2024-07-18 16:21:22 +0200

    repr print

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,32 +3,45 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self) -> str:
+        return f"{self.title} by {self.author}"
+    
+    def __repr__(self) -> str:
+        return f"Book: {self.title} by {self.author}"
 class EBook(Book):
     def __init__(self, title: str, author: str, file_size: int):
         super().__init__(title, author)
         self.file_size = file_size
-
+    
+    def __str__(self) -> str:
+        return f"{self.title} by {self.author} size: {self.file_size}"
+    
+    def __repr__(self) -> str:
+        return f"Ebook: {self.title} by {self.author}, File Size: {self.file_size}"
 
 class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
 
+    def __str__(self):
+        return f"{self.title} by {self.author} pages: {self.page_count}"
+    
+    def __repr__(self) -> str:
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
 class Library():
+    global book_list
+
     def __init__(self) -> None:
         pass
 
     def add_book(self, books):
         book_list.append(books)
 
-    def list_books(self, book_list):
-        if book_list == "classic_book":
-            print(f"Book: {self.title} by {self.author}")
-        elif book_list == "digital_novel":
-            print(f"EBook: {self.title} by {self.author}, {self.file_size}")
-        elif book_list == "paper_novel":
-            print(f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}")
-        else:
-            print("Error, invalid input")
+    def list_books(self):
+        for self.__repr__ in book_list:
+            print(self.__repr__)
+
 
 book_list = []
